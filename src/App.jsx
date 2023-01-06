a6e4d4d900e11f868d2f98a3a60856bdb161d6c3
AceDommete <68228154+AceDommete@users.noreply.github.com>
2023-01-07 00:11:28 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.scss'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Navbar/>
+      <Sidebar/>
     </div>
   )
 }
-
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.scss'
 function Navbar() {
   return (
     <div className='Navbar'>
-        <a href="/"><img src="../public/favicon.png" alt="" id='logo-nav'/></a>
+        <a href="/"><img src="./favicon.png" alt="" id='logo-nav'/></a>
             <p>File</p>
             <p>Edit</p>
             <p>View</p>
@@ -11,6 +11,10 @@ function Navbar() {
             <p>Run</p>
             <p>Terminal</p>
             <p>Help</p>
+            <span id='center-nav'>Neek Kafle - Visual Studio Code</span>
+            <span className="circleYellow"></span>
+            <span className="circleGreen"></span>
+            <span className="circleRed"></span>
     </div>
   )
 }
